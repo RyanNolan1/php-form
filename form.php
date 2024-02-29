0e625d8bbb5dd11cc49de3c2f771cb3009c98dd5
Ryan <ryannolan1990@hotmail.com>
2024-02-29 11:48:45 +0000

    this will fix an issue where the form wasn't going on to the next page even with all fields contain data

--- form.php
+++ form.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <title>kfc-form</title>
-  </head>
-  <body>
-    <div class="container">
-      <div class="logo-container">
-        <img src="./kfc-logo.svg" />
-      </div>
-      <h1>THANK YOU FOR YOUR ORDER!</h1>
-    <section class="form-section">
 <?php
-
-
-
-
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstName = htmlspecialchars($_POST["first-name"]);
     $secondName = htmlspecialchars($_POST["second-name"]);
@@ -32,47 +9,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $postCode = htmlspecialchars($_POST["post-code"]);
     $selectFood = htmlspecialchars($_POST["select-food"]);
 
-    if (empty($firstname)) {
+    if (empty($firstName) || empty($secondName) || empty($houseNumberName) || empty($street) || empty($city) || empty($county) || empty($postCode) || empty($selectFood)) {
         header("Location: ./index.php");
         exit();
     }
-    
-    if (empty($secondName)) {
-        header("Location: ./index.php");
-        exit();
-    }
-    
-    if (empty($houseNumberName)) {
-        header("Location: ./index.php");
-        exit();
-    }
-    
-    if (empty($street)) {
-        header("Location: ../index.php");
-        exit();
-    }
-    
-    if (empty($city)) {
-        header("Location: ../index.php");
-        exit();
-    }
-    
-    if (empty($county)) {
-        header("Location: ../index.php");
-        exit();
-    }
-    
-    if (empty($postCode)) {
-        header("Location: ../index.php");
-        exit();
-    }
-    
-    if (empty($selectFood)) {
-        header("Location: ../index.php");
-        exit();
-    }
+}
 ?>
-<div class="output-container">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>kfc-form</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="logo-container">
+        <img src="./kfc-logo.svg" />
+      </div>
+      <h1>THANK YOU FOR YOUR ORDER!</h1>
+    <section class="form-section">
+    <div class="output-container">
     <p><?php echo "Hello " . $firstName . ","; ?></p>
     <p><?php echo "Thank you for your order of a " . $selectFood . "!"; ?></p>
     <div class="address-container">
@@ -87,10 +48,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </p>
 </div>
 </div>
-<?php
-}
-?>
-
 </section>
   </div>
   </body>
